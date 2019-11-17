4dd6e6a04e461b02a40dd0e77463a89368c80606
Don <donli90@hotmail.com>
2019-11-16 22:57:37 -0800

    reviewed notes

--- Notes/higher-order-functions/quiz-filter-map-reduce.js
+++ Notes/higher-order-functions/quiz-filter-map-reduce.js
@@ -1,39 +1,65 @@
-// const people = [ a = { name: "jon", age: 16},
-//                  b = { name: "jane", age: 19},
-//                  c = { name: "josh", age: 20},
-//                  d = { name: "joe", age: 18} ];
+const people = [ { name: 'John',  age: 16},
+                 { name: 'Joe',   age: 19},
+                 { name: 'Jane',  age: 20},
+                 { name: 'James', age: 18} ];
 
-// const coffeeLovers = ["jon", "jane", "joe"];
+const coffeeLovers = ['John', 'Joe', 'Jane'];
 
-// const ageAbove18 = people.filter( person =>  person.age >= 18);
-// console.log(ageAbove18);
+let totaleAgeAbove18 = 0;
 
-// const addCoffeeLoverProperty = ageAbove18.map( )
+const ageAbove18 = people.filter( (person) => person.age >= 18);
+console.log(ageAbove18);
+console.log();
 
-const people = [ { name: "John Doe", age: 16 },
-                 { name: "Thomas Calls", age: 19 },
-                 { name: "Liam Smith", age: 20 },
-                 { name: "Jessy Pinkman", age: 18 }
-                ];
-const coffeeLovers = ["John Doe", "Liam Smith", "Jessy Pinkman"];
-
-const ageAbove18 = (person) => person.age >= 18;
-
-const addCoffeeLoverProperty = (person) => { 
-    person.coffeeLover = coffeeLovers.includes(person.name);
-    return person;
+const coffeeLoverProperty = (person) => {
+  person.coffeelover = coffeeLovers.includes(person.name);
+  return person;
 };
 
-const ageReducer = (sum, person) => {
-    let s = person.coffeeLover? sum + person.age: 0;
-    return s;
-}
+//people.forEach(person => coffeeLoverProperty(person));
+ageAbove18.map( person => coffeeLoverProperty(person))
 
-const coffeeLoversAbove18 = people .filter(ageAbove18) .map(addCoffeeLoverProperty);
+console.log(people);
+console.log();
 
-const totalAgeOfCoffeeLoversAbove18 = coffeeLoversAbove18 .reduce(ageReducer, 0);
+
+const ageReducer = ageAbove18.reduce( (accumulator, currentValue) => {
+  console.log(currentValue);
+  if(currentValue.coffeelover === true) {
+    totaleAgeAbove18 = accumulator + currentValue.age;
+  }
+  return totaleAgeAbove18;
+}, 0);
+
+console.log();
+console.log(totaleAgeAbove18);
+
+
+
+// const people = [ { name: "John Doe", age: 16 },
+//                  { name: "Thomas Calls", age: 19 },
+//                  { name: "Liam Smith", age: 20 },
+//                  { name: "Jessy Pinkman", age: 18 }
+//                 ];
+// const coffeeLovers = ["John Doe", "Liam Smith", "Jessy Pinkman"];
+
+// const ageAbove18 = (person) => person.age >= 18;
+
+// const addCoffeeLoverProperty = (person) => { 
+//     person.coffeeLover = coffeeLovers.includes(person.name);
+//     return person;
+// };
+
+// const ageReducer = (sum, person) => {
+//     let s = person.coffeeLover? sum + person.age: 0;
+//     return s;
+// }
+
+// const coffeeLoversAbove18 = people .filter(ageAbove18) .map(addCoffeeLoverProperty);
+
+// const totalAgeOfCoffeeLoversAbove18 = coffeeLoversAbove18 .reduce(ageReducer, 0);
  
-console.log(totalAgeOfCoffeeLoversAbove18);
+// console.log(totalAgeOfCoffeeLoversAbove18);
 
 
 // dynamic naespace for objects
