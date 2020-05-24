55b32d40aa0df72b85710a2891256cf29bbec978
Don <donli90@hotmail.com>
2020-05-24 10:50:36 -0700

    .

--- Lab/inclass-assignment/Question 11/testTree.js
+++ Lab/inclass-assignment/Question 11/testTree.js
@@ -1,21 +1,19 @@
 const fs = require('fs');
-const dirLocation = __dirname;
-const fileTrackingArr = [];
-const filePathStackArr = [];
-const fileString = 'file';
-const objArr = [];
 
-readDir(dirLocation);
+console.table(JSON.stringify(readDir(__dirname + '/test')[0], null, "  "));
+console.info(readDir(__dirname + '/test'));
 
 /**
  * Reads all files in directory
  * @param {String} path directory path
  */
-function readDir (path) {
+function readDir(path) {
   const dir = fs.readdirSync(path);
+  const arr = []
   for (ele of dir) {
-    DFSdirCheck(ele, path);
+    arr.push(DFSdirCheck(ele, path));
   }
+  return arr
 }
 
 /**
@@ -24,86 +22,39 @@ function readDir (path) {
  * @param {String} file name of each file
  * @param {String} location relative location
  */
-function DFSdirCheck (file, location) {
+function DFSdirCheck(file, location) {
   const realPath = location + `/${file}`;
   const stats = fs.lstatSync(realPath);
 
-  if(stats.isDirectory()) {
-    filePathStackArr.push(realPath);
-    fileTrackingArr.push(realPath);
-    readDir(realPath);
+  const parent = {}
+  parent.fullPath = realPath
+  parent.relativePath = file;
+  parent.name = file
+  parent.size = stats.size
+
+  if (stats.isDirectory()) {
+    parent.type = 'directory';
+    parent.children = readDir(realPath);
+  } else if (stats.isFile) {
+    parent.type = 'file'
+    parent.extension = splitter(file, '.')
+  } else {
+    parent.type = 'unknown'
   }
 
-  if(stats.isFile()) {
-    fileTrackingArr.push(realPath);
-  }
-}
-
-console.log(filePathStackArr);
-console.log(fileTrackingArr);
-
-// //returns only file objects...
-// dirReader(filePathStackArr);
-
-/**
- * Passes each directory path for object creation
- * @param {Aray} pathArr arrray of real directory paths
- */
-function dirReader (pathArr) {
-  const sortPathArr = pathArr.sort();
-  // console.log(dirLocation); //__dirname
-  // console.log(sortPathArr); //all directories in __dirname
-
-  do {
-    fileObjMaker(pathArr.pop())
-    // console.log(pathArr);
-  } while (pathArr.length > 0)
+  return parent
 }
 
 /**
- * @TODO Generates only non directory objects *** not the final large object.
- * @param {String} location array of the real directory
- */
-function fileObjMaker(location) {
-  const filesArr = fs.readdirSync(location);
-  const child = [];
-
-  for(file of filesArr) {
-    const path = location + `/${file}`;
-    const stats = fs.lstatSync(path);
-    const name = file;
-    const size = stats.size;
-    
-
-    const obj = {};
-    obj.path = path;
-    obj.name = name;
-    obj.size = size;
-
-    if(stats.isFile()) {
-      obj.extension = splitter(file, '.');
-      obj.type = fileString;
-      // console.log(obj);
-      child.push(obj);
-    }
-
-    //refactor
-    objArr.push(obj);
-  }
-}
-
-// console.log(objArr);
-
-/**
- * Finds the last occurance of index in string and returns whatever that follows it
+ * Finds the last occurrence of index in string and returns whatever that follows it
  * @param { String } str original string
  * @param { String } index split with
- * @returns { String } string following last occurance of index
+ * @returns { String } string following last occurrence of index
  */
-function splitter (str, index) {
+function splitter(str, index) {
   let extension = null;
   const extensionIndex = str.lastIndexOf(index);
-  if(extensionIndex > 0) {
+  if (extensionIndex > 0) {
     extension = str.slice(extensionIndex);
   }
   return extension;
